d67f91d395db69ab15541c0f66b03f0b23f84e8c
mariana castro <marianacastrorc@gmail.com>
2025-06-15 20:01:46 -0300

    adjusting tag resource

--- app/Http/Resources/TagResource.php
+++ app/Http/Resources/TagResource.php
@@ -18,6 +18,7 @@ class TagResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
             'name' => $this->name,
             'color' => $this->color,
         ];
